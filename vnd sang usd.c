d1b409baebf735907deba4c3ee2906ad46f2322d
nguyenmanhhieu <hieunm14092001@gmail.com>
2022-06-03 22:44:43 +0700

    chuyen doi VND sang USD
    Nguyen Manh Hiếu

---
+++ vnd sang usd.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  doi vnd sang usd
+//
+//  Created by Hiếu Hee on 03/06/2022.
+//
+
+#include <stdio.h>
+int main(void)
+{
+    float usd;
+    float vnd;
+        printf("nhap vnd\n");
+        scanf("%f",&vnd);
+    usd=vnd/23.190;
+        printf("usd is %f\n",usd);
+   return 0;
+}
